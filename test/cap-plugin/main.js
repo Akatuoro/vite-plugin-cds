1a7b8e19e22012315a94052baefedbf0343ebda2
Johannes Vogt <johannes@vogt.codes>
2025-10-27 00:02:32 +0100

    worker test

--- test/cap-plugin/main.js
+++ test/cap-plugin/main.js
@@ -7,3 +7,18 @@ const appDiv = document.getElementById('app');
 const pre = document.createElement('pre');
 pre.textContent = 'Compiled:\n\n' + JSON.stringify(csn.definitions, null, 2);
 appDiv.appendChild(pre);
+
+const worker = new Worker(new URL('./worker.js', import.meta.url), { type: 'module' });
+worker.postMessage({ id: 1, model });
+
+worker.addEventListener('message', event => {
+    const { result, error } = event.data;
+    const resultPre = document.createElement('pre');
+    if (error) {
+        resultPre.textContent = `Worker Error:\n\n${error}`;
+    }
+    else {
+        resultPre.textContent = 'Worker Compiled:\n\n' + JSON.stringify(result.definitions, null, 2);
+    }
+    appDiv.appendChild(resultPre);
+});
