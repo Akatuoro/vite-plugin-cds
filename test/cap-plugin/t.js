033c594970328d2b1424fe9bda3db1df16a9fad6
Johannes Vogt <johannes@vogt.codes>
2025-12-15 22:57:56 +0100

    add sqlite

--- test/cap-plugin/t.js
+++ test/cap-plugin/t.js
@@ -1,5 +1,6 @@
 import cds from '@sap/cds';
 import express from '../../node/polyfills/express.js';
+// import express from 'express';
 import fs from 'fs';
 
 const env = JSON.parse(fs.readFileSync('cds-env.json'));
@@ -18,6 +19,13 @@ const csn = cds.compile(model);
 // window.modules = modules
 
 const app = express();
+const o = {
+  impl: '@cap-js/sqlite',
+  credentials: { url: ':memory:' },
+  kind: 'sqlite'
+}
+cds.db = await cds.connect.to('db');
+await cds.deploy(csn).to(cds.db);
 await cds.serve('all', {}, env).from(csn).in(app);
 
 globalThis.app = app;
