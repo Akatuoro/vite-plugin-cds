548e8468d8dc8970cb049620d62087cc3229e5f1
Johannes Vogt <johannes@vogt.codes>
2025-12-16 04:58:41 +0100

    more permissive matching

--- cap/shims/preload-modules.js
+++ cap/shims/preload-modules.js
@@ -11,6 +11,8 @@ modules.resolve = (p) => {
   if (modules[p]) return p;
   if (modules[p + '.js']) return p + '.js';
 
+  p = p.replace(/\.\.\//g, '');
+
   // TODO: proper paths relative to root directory
   let full = paths.filter(p2 => p2.endsWith(p))[0];
   if (full) return full;
