d35ea5df049cf9beec764bf3a4547288702460f5
Johannes Vogt <johannes@vogt.codes>
2025-12-16 04:59:33 +0100

    fix

--- cap/esbuild.js
+++ cap/esbuild.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import fs from 'fs/promises';
 import { fileURLToPath } from 'url';
-import { insertFileDir } from './helpers';
+import { insertFileDir } from './helpers.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
