9a353d1b7a762cc2c60ded5387e449a13afaff25
Johannes Vogt <johannes@vogt.codes>
2026-01-14 23:34:10 +0100

    reintroduce worker

--- test/cap-plugin/main.js
+++ test/cap-plugin/main.js
@@ -23,20 +23,20 @@ const pre = document.createElement('pre');
 pre.textContent = 'Compiled:\n\n' + JSON.stringify(csn.definitions, null, 2);
 appDiv.appendChild(pre);
 
-// const worker = new Worker(new URL('./worker.js', import.meta.url), { type: 'module' });
-// worker.postMessage({ id: 1, model });
+const worker = new Worker(new URL('./worker.js', import.meta.url), { type: 'module' });
+worker.postMessage({ id: 1, model });
 
-// worker.addEventListener('message', event => {
-//     const { result, error } = event.data;
-//     const resultPre = document.createElement('pre');
-//     if (error) {
-//         resultPre.textContent = `Worker Error:\n\n${error}`;
-//     }
-//     else {
-//         resultPre.textContent = 'Worker Compiled:\n\n' + JSON.stringify(result.definitions, null, 2);
-//     }
-//     appDiv.appendChild(resultPre);
-// });
+worker.addEventListener('message', event => {
+    const { result, error } = event.data;
+    const resultPre = document.createElement('pre');
+    if (error) {
+        resultPre.textContent = `Worker Error:\n\n${error}`;
+    }
+    else {
+        resultPre.textContent = 'Worker Compiled:\n\n' + JSON.stringify(result.definitions, null, 2);
+    }
+    appDiv.appendChild(resultPre);
+});
 
 const response = await serve();
 const responsePre = document.createElement('pre');
